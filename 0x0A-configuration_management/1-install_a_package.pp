838d7b7d87fde8a3842c469c622765f5963a093c
aya-cs22 <aya786930@gmail.com>
2024-04-22 09:37:17 +0200

    Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,21 +1,16 @@
-# Using Puppet, install pip3.
-package{'pip3':
-    ensure    => installed,
+#Using Puppet, install flask from pip3.
+package{'python3':
+    ensure    => '3.8.10',
+    provider => 'pip3',  
+}
+package{'flask':
+    ensure    => '2.1.0',
+    provider => 'pip3',
+    require   => Package['python3'],
 }
 
-# Ensure Python3 is installed.
-package{'python3':
-    ensure    => installed,
-}
-# Using Puppet, install flask from pip3.
-package{'flask':
-    ensure       => 'present',
-    provider     => 'pip3',  
-    require      => ['Package[python3]'],
-}
-# Ensure Werkzeug is installed with a compatible version.
 package{'Werkzeug':
-    ensure    => '2.1.1',
-    provider => 'pip3',  
-    require   => ['Package[python3]'],
+    ensure    => '2.1.0',
+    provider => 'pip3',
+    require   => Package['python3'],
 }
