aac9a7b41d846c74c8908db60cb6b0c1630826a5
aya-cs22 <aya786930@gmail.com>
2024-06-11 04:12:49 +0000

    Increase the amount of traffic the Nginx server can handle.

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin:/bin',
+  notify  => Exec['nginx-restart'],
+}
+exec { 'nginx-restart':
+  command => 'service nginx restart',
+  path    => ['/etc/init.d'],
+}
